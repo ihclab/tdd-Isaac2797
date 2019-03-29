29d8ff3c306d61ac70eb006ec8845d5cec047bd2
Isaac <iramirez6@ucol.mx>
2019-03-29 00:37:17 -0600

    Casos de pruebas agregados

--- Ordenamiento/Burbuja.cs
+++ Ordenamiento/Burbuja.cs
@@ -5,7 +5,13 @@ namespace Ordenamiento
     public class Burbuja
     {
         public void Ordenar(int[] arr) {
-            throw new NotImplementedException("Primero las pruebas");
+            for (int i = 0; i < arr.Length - 1; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    int aux = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = aux;
+                }
+            }
         }
     }
 }
